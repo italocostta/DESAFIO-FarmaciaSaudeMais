071f60c18d61706c40f3579320a5de7135a186c5
italocostta <italo.costta26@gmail.com>
2025-08-22 17:54:25 -0300

    cfg TypeORM conexao com postgres e variaveis no env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { Funcionario } from './funcionario/funcionario.entity';
+import { Cliente } from './cliente/cliente.entity';
+import { Remedio } from './remedio/remedio.entity';
+import { FuncionarioModule } from './funcionario/funcionario.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { RemedioModule } from './remedio/remedio.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        type: 'postgres',
+        host: cfg.get<string>('DB_HOST'),
+        port: +cfg.get<number>('DB_PORT'),
+        username: cfg.get<string>('DB_USER'),
+        password: cfg.get<string>('DB_PASS'),
+        database: cfg.get<string>('DB_NAME'),
+        entities: [Funcionario, Cliente, Remedio],
+        synchronize: true, // deixe true só em DEV
+      }),
+    }),
+    FuncionarioModule,
+    ClienteModule,
+    RemedioModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
