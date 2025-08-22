16662544cc3766ce36c54ef31a9b2084ce829eda
italocostta <italo.costta26@gmail.com>
2025-08-22 18:01:16 -0300

    add roles decorators e roles guard para autenticacao

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(ctx: ExecutionContext): boolean {
+    const required = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      ctx.getHandler(),
+      ctx.getClass(),
+    ]);
+    if (!required?.length) return true;
+
+    const req = ctx.switchToHttp().getRequest();
+    // Trocar por JWT depois; por ora, simulado:
+    const user = req.user ?? { role: 'ATENDENTE' };
+    if (!required.includes(user.role))
+      throw new ForbiddenException('Acesso negado');
+    return true;
+  }
+}
